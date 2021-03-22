532a29f799eaf3a15e1ff178535862899cb54d7c
Antonio <AntonioGMMelo@yahoo.com.br>
2021-03-22 13:47:27 +0000

    Added Dummy package

---
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/Dummy/DummyClass2.java
@@ -0,0 +1,78 @@
+package ES_2Sem_2021_Grupo53.ES_2Sem_2021_Grupo53.Dummy;
+
+//Dummy Class to Test Number Of Lines Metrics
+public class DummyClass2 {
+	
+	//Dummy Class for Small Number of Lines of Code
+	private static void dummyTiny() {
+		
+		System.out.println("Hello World!");
+		
+	}
+	
+	//Dummy Class for High Number of empty Lines of Code
+	private static void dummySpace() {
+		
+		
+		
+		
+		
+		System.out.println("Hello World");
+		
+		
+		
+		
+		
+	}
+	
+	//Dummy Class for Medium Number of Lines of Code
+	private static void dummyMedium() {
+		
+		System.out.println("H");
+		System.out.println("e");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("o");
+		
+		System.out.println("W");
+		System.out.println("o");
+		System.out.println("r");
+		System.out.println("l");
+		System.out.println("d");;
+		
+	}
+	
+	//Dummy Class for High Number of Lines of Code
+	private static void dummyLong() {
+		
+		System.out.println("H");
+		System.out.println("e");
+		System.out.println("l");
+		System.out.println("l");
+		System.out.println("o");
+		
+		System.out.println("W");
+		System.out.println("o");
+		System.out.println("r");
+		System.out.println("l");
+		System.out.println("d");
+		
+		System.out.println("T");
+		System.out.println("h");
+		System.out.println("i");
+		System.out.println("s");
+		
+		System.out.println("I");
+		System.out.println("s");
+
+		System.out.println("L");
+		System.out.println("o");
+		System.out.println("n");
+		System.out.println("g");
+		
+		System.out.println("A");
+		System.out.println("F");
+	
+	}
+	
+}
