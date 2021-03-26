43ffe0de82072c9819bee1690cf0fe3113d14f17
Miguel Lourenço <miguelplourenco@gmail.com>
2021-03-26 19:14:10 +0000

    just so i can pull

--- src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/Assert.java
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/Assert.java
@@ -9,8 +9,8 @@ public class Assert {
 	@Test
 	public void testAssertion(){
 		
-		String PATH = "C:\\Users\\Ant\\eclipse-workspace\\ES-2Sem-2021-Grupo53\\src\\main\\java\\ES_2Sem_2021_Grupo53\\ES_2Sem_2021_Grupo53\\Dummy\\DummyClass.java";		
-		String PATH2 = "C:\\Users\\Ant\\eclipse-workspace\\ES-2Sem-2021-Grupo53\\src\\main\\java\\ES_2Sem_2021_Grupo53\\ES_2Sem_2021_Grupo53\\Dummy\\DummyClass2.java";
+		String PATH = "C:\\Users\\migue\\eclipse-workspace\\ES-2Sem-2021-Grupo53\\src\\main\\java\\ES_2Sem_2021_Grupo53\\ES_2Sem_2021_Grupo53\\Dummy\\DummyClass.java";		
+		String PATH2 = "C:\\Users\\migue\\eclipse-workspace\\ES-2Sem-2021-Grupo53\\src\\main\\java\\ES_2Sem_2021_Grupo53\\ES_2Sem_2021_Grupo53\\Dummy\\DummyClass2.java";
 		
 		int answer1 = new Metrics().getMetrics(PATH); 
 		int answer2 = new Metrics().getMetrics(PATH2);
