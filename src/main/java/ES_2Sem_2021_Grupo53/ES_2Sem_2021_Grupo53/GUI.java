3d0826baa517e04754b206bee4f1e07bd0cd4f4f
mjlsv-iscte <mjlsvd@gmail.com>
2021-04-02 15:57:56 +0100

    Adicionar stuff à classe GUI

--- src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
@@ -1,5 +1,40 @@
 package ES_2Sem_2021_Grupo53.ES_2Sem_2021_Grupo53;
 
-public class GUI {
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class GUI extends Application{
+
+	Button button;
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		primaryStage.setTitle("GUI");
+		
+		button = new Button();
+		button.setText("Get Path");
+		
+		StackPane layout = new StackPane();
+		layout.getChildren().add(button);
+		
+		Scene scene = new Scene(layout, 300, 250);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+		
+	}
+	
+	public void main(String[] args) {
+		launch(args);
+	}
 
 }
--- src/test/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/Main.java
+++ src/test/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/Main.java
@@ -20,6 +20,8 @@ public class Main {
 		
 		System.out.println("Succesful: " + result.wasSuccessful());
 		
+	
+		
 	}
 
 }
