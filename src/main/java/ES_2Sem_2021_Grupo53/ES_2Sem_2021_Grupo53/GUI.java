f270480e603bbfdd47776a736f13ee6d7c29c046
mjlsv-iscte <mjlsvd@gmail.com>
2021-04-04 15:05:07 +0100

    Update da GUI

--- src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
@@ -1,39 +1,65 @@
 package ES_2Sem_2021_Grupo53.ES_2Sem_2021_Grupo53;
 
-
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.scene.Group;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Stage;
 
 public class GUI extends Application{
 
-	Button button;
+	Label metrics;
+	Label lOperator;
+	Label limits;
+	
+	ComboBox metriCB;
+	ComboBox cb1;
+	
+	TextField mLimits;
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		
-		primaryStage.setTitle("GUI");
+		primaryStage.setTitle("Code Smells");
 		
-		button = new Button();
-		button.setText("Get Path");
+		metrics = new Label("Choose Metrics");
+		metriCB = new ComboBox();
 		
-		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		limits = new Label("Choose limits");
+		mLimits = new TextField();
 		
-		Scene scene = new Scene(layout, 300, 250);
+		String logicOp[] =
+            { "AND", "OR" };
+		
+		lOperator = new Label("Choose Logic Operator");
+		cb1 = new ComboBox(FXCollections.observableArrayList(logicOp));
+		
+		GridPane grid = new GridPane();
+		grid.setHgap(10);
+		grid.setVgap(10);
+		
+		grid.add(metrics, 1, 1);
+		grid.add(limits, 1, 2);
+		grid.add(mLimits, 2, 2);
+		grid.add(lOperator, 1, 3);
+		
+		grid.add(metriCB, 2, 1);
+		grid.add(cb1, 2, 3);
+		
+		
+		Scene scene = new Scene(grid, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
 	}
 	
-	public void main(String[] args) {
+	public static void main(String[] args) {
 		launch(args);
 	}
 
