26477ab770fda917a2602d780ed436c87018fe83
mjlsv-iscte <mjlsvd@gmail.com>
2021-04-04 19:59:01 +0100

    Update2 GUI

--- src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/GUI.java
@@ -1,9 +1,9 @@
 package ES_2Sem_2021_Grupo53.ES_2Sem_2021_Grupo53;
 
 import javafx.application.Application;
-<<<<<<< HEAD
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.text.Font;
@@ -11,15 +11,20 @@ import javafx.scene.text.Text;
 import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
-=======
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
->>>>>>> 6727ebc4e7ae9e84fe8e1188d74510961c733272
 import javafx.stage.Stage;
 
 public class GUI extends Application{
 
+	GridPane grid = new GridPane();
+	Scene scene = new Scene(grid, 300, 250);
+	
+	Stage secondaryStage = new Stage();
+	GridPane secGrid = new GridPane();
+	Scene secondScene = new Scene(secGrid, 200, 100);
+	
 	Label metrics;
 	Label lOperator;
 	Label limits;
@@ -27,7 +32,31 @@ public class GUI extends Application{
 	ComboBox metriCB;
 	ComboBox cb1;
 	
-	TextField mLimits;
+	Button choose;
+	
+	/**
+	 * This button has to redirect the user to a form 
+	 * so he can set limits of the metric chosen!.
+	 * */
+	@SuppressWarnings("unchecked")
+	private void chooseButtonAction() {
+		choose.setOnAction(new EventHandler() {
+			@Override
+			public void handle(Event event) {
+				secondaryStage.setTitle("Choose Limits");
+				Label metChosen = new Label("Metric chosen: ");				
+				secGrid.add(metChosen, 1, 1);
+				
+				Spinner choice = new Spinner(0, 1000, 0);
+			    choice.setEditable(true);
+				secGrid.add(choice, 1, 2);
+			
+				
+				secondaryStage.setScene(secondScene);
+				secondaryStage.show();
+			}
+		});
+	}
 	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
@@ -38,7 +67,8 @@ public class GUI extends Application{
 		metriCB = new ComboBox();
 		
 		limits = new Label("Choose limits");
-		mLimits = new TextField();
+		choose = new Button("Go!");
+		chooseButtonAction();
 		
 		String logicOp[] =
             { "AND", "OR" };
@@ -46,20 +76,18 @@ public class GUI extends Application{
 		lOperator = new Label("Choose Logic Operator");
 		cb1 = new ComboBox(FXCollections.observableArrayList(logicOp));
 		
-		GridPane grid = new GridPane();
+		
 		grid.setHgap(10);
 		grid.setVgap(10);
 		
 		grid.add(metrics, 1, 1);
 		grid.add(limits, 1, 2);
-		grid.add(mLimits, 2, 2);
+		grid.add(choose, 2, 2);
 		grid.add(lOperator, 1, 3);
 		
 		grid.add(metriCB, 2, 1);
 		grid.add(cb1, 2, 3);
 		
-		
-		Scene scene = new Scene(grid, 300, 250);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
