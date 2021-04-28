132c6767f8c527c9a4434a5fef44fc832f1fe945
AntonioGMMelo <AntonioGMMelo@yahoo.com.br>
2021-04-28 15:55:17 +0100

    furtherd GUI

--- src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/MyGUI.java
+++ src/main/java/ES_2Sem_2021_Grupo53/ES_2Sem_2021_Grupo53/MyGUI.java
@@ -40,6 +40,7 @@ public class MyGUI extends Application {
 	}
 	
 	
+	@SuppressWarnings("unchecked")
 	@Override
 	public void start(final Stage primaryStage) throws Exception {
 
@@ -49,7 +50,7 @@ public class MyGUI extends Application {
 		//First smell definition
 		final GridPane gridHelper = new GridPane();
 		
-		String[] smells = new String[] {"is_God_CLass", "is_Long_Method"};
+		String[] smells = new String[] {"is_God_Class", "is_Long_Method"};
 		final ComboBox smell = new ComboBox(FXCollections.observableArrayList(smells));
 		
 		gridHelper.add(smell, 0, 0);
@@ -61,87 +62,111 @@ public class MyGUI extends Application {
 
 			@Override
 			public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
-				
-				String[] methodsMetrics = methodsMetricHelper;
-				String[] classMetrics = classMetricsHelper;
-				GridPane grid = gridHelper;
+			
+				final String[] classMetricsHelper2 = classMetricsHelper;
+				final GridPane grid = gridHelper;
 				
 				if(newValue.toString().equals("is_God_Class")) {
 					
-					ComboBox metrics = new ComboBox(FXCollections.observableArrayList(newValue.toString().equals("is_God_Class")));
+					final ComboBox metricsHelper = new ComboBox(FXCollections.observableArrayList(classMetricsHelper2));
 				
-					grid.add(metrics, 2, 0);
+					grid.add(metricsHelper, 0, 2);
 				
-					if(metrics.getValue().equals("LOC_Class")) {
-					
-						metrics.setDisable(true);
-						classOrder.add("LOC_Class");
-						classMetrics = remove(classMetrics,"LOC_Class");
-					
-					}else if(metrics.getValue() == "NOM_Class") {
+					metricsHelper.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+						@Override
+						public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
 						
-						metrics.setDisable(true);
-						classOrder.add("NOM_Class");
-						classMetrics = remove(classMetrics,"NOM_Class");	
-					
-					}else if(metrics.getValue().equals("WMC_Class")) {
-					
-						metrics.setDisable(true);
-						classOrder.add("WMC_Class");
-						classMetrics = remove(classMetrics,"WMC_Class");
-					
-					}
-				
-					TextField threshold1 = new TextField();
-					threshold1.setPromptText("Threshold");
-				
-					try {
-					
-						int helper = Integer.parseInt(threshold1.getText());
-						threshold1.setDisable(true);
-						classThreshold.add(helper);
-					
-					}catch(Exception e) {
-						//Error Message pop up to do
-					}
-				
-					if(threshold1.isDisabled()) {
-					
-						ComboBox logic = new ComboBox(FXCollections.observableArrayList(new String[]{"AND", "OR"}));
-				
-						if(logic.getValue().toString().equals("AND")) {
-					
-							logic.setDisable(true);
-							classLogic.add("AND");
-					
-						}else if(logic.getValue().toString().equals("OR")) {
-					
-							logic.setDisable(true);
-							classLogic.add("OR");
-					
+							 String[] classMetrics = classMetricsHelper2;
+							 ComboBox metrics = metricsHelper;
+							 
+							if(newValue.equals("LOC_Class")) {
+			
+								classOrder.add("LOC_Class");
+								classMetrics = remove(classMetrics,"LOC_Class");
+							
+							}else if(newValue.equals("NOM_Class")) {
+								
+								classOrder.add("NOM_Class");
+								classMetrics = remove(classMetrics,"NOM_Class");	
+							
+							}else if(newValue.equals("WMC_Class")) {
+							
+								classOrder.add("WMC_Class");
+								classMetrics = remove(classMetrics,"WMC_Class");
+							
+							}
+						
+							TextField threshold1 = new TextField();
+							threshold1.setPromptText("Threshold");
+							grid.add(threshold1, 1, 2);
+						
+							//try {
+							
+								//int helper = Integer.parseInt(threshold1.getText());
+								//threshold1.setDisable(true);
+								//classThreshold.add(helper);
+							
+						//	}catch(Exception e) {
+								//Error Message pop up to do
+							//}
+						
+							//if(threshold1.isDisabled()) {
+							
+								//ComboBox logic = new ComboBox(FXCollections.observableArrayList(new String[]{"AND", "OR"}));
+						
+								//if(logic.getValue().toString().equals("AND")) {
+							
+									//logic.setDisable(true);
+									//classLogic.add("AND");
+							
+								//}else if(logic.getValue().toString().equals("OR")) {
+							
+									//logic.setDisable(true);
+									//classLogic.add("OR");
+							
+								//}
+							
+							//}
+							
 						}
 					
-					}
+					});
 				
 				}else if(smell.getValue().toString().equals("is_Long_Method")) {
 				
-					ComboBox metrics = new ComboBox(FXCollections.observableArrayList(smell.getValue().toString().equals("is_Long_Method")));
-				
-					grid.add(metrics, 2, 0);
-				
-					if(metrics.getValue().equals("LOC_Method")) {
+					final String[] methodsMetricsHelper2 = methodsMetricHelper;
 					
-						metrics.setDisable(true);
-						methodOrder.add("LOC_Method");
-						methodsMetrics = remove(methodsMetrics,"LOC_Method");
-					
-					}else if(metrics.getValue().equals("CYCLO_Method")) {
+					final ComboBox metricsHelper = new ComboBox(FXCollections.observableArrayList(methodsMetricsHelper2));
+				
+					grid.add(metricsHelper, 0, 2);
+				
+
+					metricsHelper.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+
+						ComboBox metrics = metricsHelper;
 						
-						metrics.setDisable(true);
-						classOrder.add("CYCLO_Method");
-						methodsMetrics = remove(methodsMetrics,"CYCLO_Method");	
-					
-					}
+						@Override
+						public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+						
+							String[] methodsMetrics = methodsMetricsHelper2;
+							
+							if(newValue.equals("LOC_Method")) {
+							
+								methodOrder.add("LOC_Method");
+								methodsMetrics = remove(methodsMetrics,"LOC_Method");
+							
+							}else if(newValue.equals("CYCLO_Method")) {
+								
+								classOrder.add("CYCLO_Method");
+								methodsMetrics = remove(methodsMetrics,"CYCLO_Method");	
+							
+							}
+							
+						} 
+						
+					});
+
 				
 				}
 				
